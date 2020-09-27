5742d89e190d98f31c8dea1a1a0734a14fecd9a5
Julio Nepomuceno <julioreplay@gmail.com>
2020-09-27 13:50:19 -0300

    Adicionando sistema de vitória, tratamento de derrota se o usuário errar mais de 5 vezez, tratamento de erro se o usuário colocar mais de 5 palavras, e por ultimo o sistema de wordStatus foi adicionado

--- src/script.js
+++ src/script.js
@@ -1,16 +1,55 @@
 const pageMain = document.getElementById('page-main')
 const wordPage = document.getElementById('word')
 const letterPage = document.getElementById('letter')
-const wordStatus = document.getElementById('wordStatus')
 const answer = document.getElementById('answer')
+const wordStatus = document.getElementById('wordStatus')
+
 let arrayWord = []
 let joinArrayWord = []
 
+let wordsCompletOfPhrase = []
+function phraseVerification() { // Verifica se ele colocou mais de 5 palavras no input.
+    wordsCompletOfPhrase = (wordPage.value).split(' ')
+    if (wordsCompletOfPhrase.length > 5 ) {
+        arrayWord = [] // Se tiver mais que 5 palavras ele limpa todos os arrays e o answer
+        joinArrayWord = []
+        wordsCompletOfPhrase = []
+        answer.value = ''        
+
+        alert('Você só pode colocar até 5 palavras.')
+    }
+}
+
+let qttLetters
 function addWord() {
-    arrayWord = []
+    
+    arrayWord = [] // Toda vez que apertarmos esse botão ele primeiramente ele irá limpar todos os arrays(answer também) se caso eles tiverem algo.
     joinArrayWord = []
+    wordsCompletOfPhrase = []
     answer.innerHTML = ''
+    
     arrayWord = (wordPage.value).split('')
+
+    qttLetters = Array(arrayWord.length)
+    const repleceableArray = Array(qttLetters.length)
+
+    for (let i = 0 ; i < qttLetters.length ; i++) {
+        if (qttLetters[i] === undefined) {
+            qttLetters[i] = ' _ '
+            repleceableArray[i] = ' _ '
+        } 
+    }
+
+    arrayWord.forEach( (e, i) => { // Se tiver algum espaço nas palvras que o user mandou eu vou setar esse espaço, no novo array
+        if (e === ' ') {
+            repleceableArray[i] = 'space'
+        } 
+    })
+
+    wordStatus.innerHTML = repleceableArray.join('').replace('space' , '&nbsp')
+
+
+    phraseVerification()
 }
 
 const treatmentOfErro = []
@@ -23,12 +62,14 @@ function treatmentError(error) { // Validação de erro
 }
 
 function victory() {
-    const compare = (element, indice) => element === joinArrayWord[indice]
-    const hasWon = arrayWord.every(compare) 
-    
-    if(hasWon === true) {
-        pageMain.innerHTML = 'Ganhou'
-    }
+    setTimeout( () => {
+        const compare = (element, indice) => element === joinArrayWord[indice]
+        const hasWon = arrayWord.every(compare) 
+        
+        if(hasWon === true) {
+            pageMain.innerHTML = 'Ganhou'
+        }
+    },2000)
 }
 
 function testLetter() {
@@ -41,22 +82,30 @@ function testLetter() {
             hit = `A letra ${arrayWord[i]} está correta.`
             joinArrayWord[i] = letterMoment // A letra que vai entrar no joinArrayWord será a letra do momento que está no arrayWord.
 
+            if (hit) {
+                qttLetters[i] = ` ${letterMoment} `
+         }
+
+
         } else {
             mistake = `ERROR a letra ${letter} não está correta`
         }
     })
-
+    
     arrayWord.forEach( (e, i) => { // Se tiver algum espaço nas palvras que o user mandou eu vou setar esse espaço, no novo array
         if (e === ' ') {
             joinArrayWord[i] = ' '
+            qttLetters[i] = 'space'
+        } else if (e === '  ') {
+            joinArrayWord[i] = '  '
         }
     })
 
-    if(!hit) treatmentError('error') // se o hit for false será chamada a callback de erro
-    victory() // Vai chamar a callback victory pra checar se o user já ganhou
+    if(!hit) treatmentError('error') // se o hit for false será chamada a callback de erro.
+    victory() // Vai chamar a callback victory pra checar se o user já ganhou.
 
     answer.innerHTML = `${hit? hit : mistake}<br><br>`
-    answer.innerHTML += joinArrayWord.join('')
+    wordStatus.innerHTML = qttLetters.join('').replace('space' , '&nbsp')
 
     letterPage.value = ''
     letterPage.focus()
