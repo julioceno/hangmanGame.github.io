aafac5642a9570c24f3215acd93f465c901b75d4
Julio Nepomuceno <julioreplay@gmail.com>
2020-09-29 17:18:06 -0300

    Antes era aplicado apenas um espaço corretamnete, os 'spaces' poseriores não eram modificados pelo replace, por isso eu fiz um forEach nos arrays que precisavam dessa funcionalidade adicionando o espaço corretamente em todo lugar preenchido pelo space. Além disso adicionei o normalize, que ignora açentos de letras, deixando o usuário colocar um 'c' e esse ser igual ao 'ç'.

--- src/script.js
+++ src/script.js
@@ -30,11 +30,11 @@ function addWord() {
     
     arrayWord = (wordPage.value).split('')
 
-    qttLetters = Array(arrayWord.length)
-    const repleceableArray = Array(qttLetters.length)
+    qttLetters = Array(arrayWord.length) ;; // Esse array é criado com a quantidade de elementos da palavra colocada pelo user, unicamente para colocar os _ mas ele só funciona quando a função testLetter é chamada.
+    const repleceableArray = Array(qttLetters.length) // Esse array tem quase o mesmo sentido do anterior, poém assim que essa função é chamado ele já coloca os _ porém quandoeu invoco o testLetter ele é meio que substituido.
 
     for (let i = 0 ; i < qttLetters.length ; i++) {
-        if (qttLetters[i] === undefined) {
+        if (qttLetters[i] === undefined) { // Toda vez que a posição for  indefinida será setado um _, pra os dois arrays.
             qttLetters[i] = ' _ '
             repleceableArray[i] = ' _ '
         } 
@@ -42,12 +42,18 @@ function addWord() {
 
     arrayWord.forEach( (e, i) => { // Se tiver algum espaço nas palvras que o user mandou eu vou setar esse espaço, no novo array
         if (e === ' ') {
-            repleceableArray[i] = 'space'
+            repleceableArray[i] = 'space' // Explicação na função testLetter próximo ao fim.
         } 
+        console.log(repleceableArray[i])
     })
-
-    wordStatus.innerHTML = repleceableArray.join('').replace('space' , '&nbsp')
-
+ 
+    repleceableArray.forEach( (e, i) => {
+        if (e === 'space') {
+            repleceableArray[i] = '&nbsp'
+        }
+    })
+ 
+    wordStatus.innerHTML =  repleceableArray.join('')
 
     phraseVerification()
 }
@@ -77,15 +83,14 @@ function testLetter() {
     let hit 
     let mistake
 
-    arrayWord.forEach( (letterMoment, i) => {
-        if (letter.toUpperCase() == letterMoment.toUpperCase()) { // A letra que o user mandou será passada pra maiúscula assim como a letra do momento, então se a letra for passada maiusucla ou minusula será true 
-            hit = `A letra ${arrayWord[i]} está correta.`
+    arrayWord.forEach( (letterMoment, i) => {// Comentário na linha posterior
+        if (letter.toUpperCase().normalize('NFD').replace(/[\u0300-\u036f]/g , '') == letterMoment.toUpperCase().normalize('NFD').replace(/[\u0300-\u036f]/g , '')) { // A letra que o user mandou será passada pra maiúscula assim como a letra do momento, então se a letra for passada maiusucla ou minusula será true 
+            hit = `A letra ${arrayWord[i]} está correta.`                                                                                                             //A função normalize será aplicada a todos as letras que tiverem açentos, aplicando tanto em letter como em letterMoment e será ignorado todas os açentos pois os dois serão iguais.                                                                          
             joinArrayWord[i] = letterMoment // A letra que vai entrar no joinArrayWord será a letra do momento que está no arrayWord.
 
             if (hit) {
                 qttLetters[i] = ` ${letterMoment} `
          }
-
 
         } else {
             mistake = `ERROR a letra ${letter} não está correta`
@@ -95,16 +100,23 @@ function testLetter() {
     arrayWord.forEach( (e, i) => { // Se tiver algum espaço nas palvras que o user mandou eu vou setar esse espaço, no novo array
         if (e === ' ') {
             joinArrayWord[i] = ' '
-            qttLetters[i] = 'space'
+            qttLetters[i] = 'space' // Esse "space" que eu dou nos espaços do array será aplicado um replace em cima deles e no lugar do space será dado um espaço pra separar as palavras.
         } else if (e === '  ') {
             joinArrayWord[i] = '  '
         }
     })
+
     if(!hit) treatmentError('error') // se o hit for false será chamada a callback de erro.
     victory() // Vai chamar a callback victory pra checar se o user já ganhou.
 
+    qttLetters.forEach( (e, i) => {
+        if (e === 'space') {
+            qttLetters[i] = '&nbsp'
+        }
+    })
+
     answer.innerHTML = `${hit? hit : mistake}<br><br>`
-    wordStatus.innerHTML = qttLetters.join('').replace('space' , '&nbsp')
+    wordStatus.innerHTML = qttLetters.join('')
 
     letterPage.value = ''
     letterPage.focus()
