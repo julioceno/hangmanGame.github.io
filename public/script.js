5834650bea87f5fad10599c64d72edb04d24052d
Julio Nepomuceno <julioreplay@gmail.com>
2020-10-27 09:36:07 -0300

    fazendo o teste se a letra que é passada pelo usuário é igual a letras da palavra, fazendo melhorias no código e simplificando ele

--- public/script.js
+++ public/script.js
@@ -1,11 +1,13 @@
-const wordPage = document.getElementById('word')
+const wordPage = document.getElementById('wordPage')
+const letterPage = document.getElementById('letterPage')
+const statusLetter = document.getElementById('status-letter')
 const traceContainer = document.getElementById('trace-container')
-const letterPage = document.getElementById('letter')
+const letterContainer = document.getElementById('letter-container')
 
 const dissappearGameConfig = document.getElementById('dissappearGameConfig') //Pegando conteúdo que deve aparecer e desaparecer
 const appearContentLetter = document.getElementById('appearContentLetter')
 appearContentLetter.style.display = "none" 
-let wordCorret;
+
 let arrayWord;
 function addWord() {
    
@@ -30,49 +32,49 @@ function addWord() {
     arrayWord = (wordPage.value).split("")
      const addingTraces = Array(arrayWord.length) // isso vai retornar um array com a mesma quantidade de letras que o usuário adicionou, mas as posições do array serão undefined
      
+
+
      for (let i = 0 ; i < addingTraces.length ; i++) { // adicionando as imagens no novo array criado.
         if (addingTraces[i] === undefined) { 
-            addingTraces[i] = ' <img class="trace" src="/public/images/stickman/gallows-trace.png" alt="traço"> '
+            addingTraces[i] = ' <div class="trace"></div> '
         } 
     }  
-
-    wordCorret = Array(arrayWord.length)
-    for (let i = 0 ; i < wordCorret.length ; i++) { // vai trocar todos os undefined's por um espaço vazio
-        if (wordCorret[i] === undefined) { 
-            wordCorret[i] = ''
-        } 
-    }
+    
     traceContainer.innerHTML = addingTraces.join('')
 }
 
-
-function testLetter() {
+let wordCorret = [];
+function addLetter() {
 
     const letter = letterPage.value
     let hit;
     let mistake;
+    let currentIndice
+    let arrayIndice = []
 
     arrayWord.forEach( (e, i) => {
-        if (letter.toUpperCase().normalize('NFD').replace(/[\u0300-\u036f]/g , '') === e.toUpperCase().normalize('NFD').replace(/[\u0300-\u036f]/g , '')) {
-            
+        if (letter.toUpperCase().normalize('NFD').replace(/[\u0300-\u036f]/g , '') === e.toUpperCase().normalize('NFD').replace(/[\u0300-\u036f]/g , '')) {     
+            hit = arrayWord[i]
+            currentIndice = i
+            arrayIndice.push(i)
 
-            wordCorret[i] += `<p> ${arrayWord[i]} </p>`
-            hit = letter
 
         } else {
              mistake = `[ERRO] a letra ${letter}` 
         }
     })
 
-    traceContainer.innerHTML = wordCorret.join('')
+    const addLetterInIndiceCorrect = e =>  wordCorret[e] = `<div class="letter">${hit}</div>` // se o usuário colocar uma palavra que tenha letras iguais eu vou pegar os indíces dessa letra e vou ir adicionando a letra nesses indices
+    hit? arrayIndice.forEach(addLetterInIndiceCorrect) : wordCorret // todos vinham concatenados com undefined e também quando se errava uma letra entrava o undfined no array. com esse algoritmo a letra só é adicionada se o hit for true
 
+    console.log(wordCorret)
+    letterContainer.innerHTML = wordCorret.join('')
 
     letterPage.value = ''
     letterPage.focus()
 }
 
-
-(function() {
+(function() { // algoritmo que joga uma cor aleátoria para o body
     const colors = {
         0: '#b8de6f',1: '#388e3c', 2: '#1565c0',3: '#e53935',4: '#f67809',5: '#5e35b1',
         6: '#fbc02d',7: '#d81b60',8: '#64dd17',9: '#00acc1',10: '#304ffe',11: '#9f6581',
